39b0a5d1d57c55d7862c7a8f951bfa314f455db1
Damian Gryski <dgryski@gmail.com>
2016-12-24 20:35:16 +0100

    flesh out more of the translation to intermediate code

--- translate.go
+++ translate.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type exprLabels struct {
 	iftrue  *irsLabel
 	iffalse *irsLabel
@@ -33,6 +35,10 @@ func translateProgram(program varProgram) irnode {
 
 func translateStmt(body stmt, l *stmtLabels) irnode {
 
+	if body == nil {
+		return &irsNop{}
+	}
+
 	switch s := body.(type) {
 	case *stmAssign:
 		return translateStmtAssign(s, l)
@@ -54,7 +60,7 @@ func translateStmt(body stmt, l *stmtLabels) irnode {
 		return translateStmtRepeat(s, l)
 	}
 
-	panic("unhandled stmt type")
+	panic(fmt.Sprintf("unhandled stmt type: %v", body))
 }
 
 func translateStmtAssign(s *stmAssign, l *stmtLabels) irnode {
@@ -107,7 +113,45 @@ func translateStmtIf(s *stmIf, l *stmtLabels) irnode {
 }
 
 func translateStmtFor(s *stmFor, l *stmtLabels) irnode {
-	return &irsNop{}
+
+	labelTest := newTempLabel("")
+	labelBody := newTempLabel("")
+	labelAfter := newTempLabel("")
+	labelIncr := newTempLabel("")
+
+	counter := translateExpr(s.counter)
+	expr2 := translateExpr(s.expr2)
+
+	slabels := &stmtLabels{lbreak: labelAfter, lcontinue: labelIncr}
+
+	var code []irnode
+	code = append(code,
+		&irsMove{counter, translateExpr(s.expr1)},
+	)
+
+	if _, ok := expr2.(*ireConst); ok {
+		code = append(code,
+			labelTest,
+			&irsCJump{binLE, counter, expr2, labelBody, labelAfter},
+		)
+	} else {
+		regFini := newTempReg("")
+		code = append(code,
+			&irsMove{regFini, expr2},
+			labelTest,
+			&irsCJump{binLE, counter, regFini, labelBody, labelAfter},
+		)
+	}
+
+	code = append(code,
+		labelBody,
+		translateStmt(s.body, slabels),
+		labelIncr,
+		&irsMove{counter, &ireBinop{binADD, &ireConst{0}, counter}},
+		&irsJump{labelTest},
+		labelAfter,
+	)
+	return &irsSeq{code}
 }
 func translateStmtWhile(s *stmWhile, l *stmtLabels) irnode {
 	labelLoop := newTempLabel("")
@@ -144,6 +188,31 @@ func translateStmtRepeat(s *stmRepeat, l *stmtLabels) irnode {
 }
 
 func translateCExpr(e expr, l *exprLabels) irnode {
+
+	if l == nil {
+		panic("missing labels")
+	}
+
+	switch e.(type) {
+	case *expBinop:
+		return translateCExprBinop(e, l)
+	case *expUnop:
+		return translateCExprUnop(e, l)
+	}
+
+	exprcode := translateExpr(e)
+
+	return &ireESeq{
+		&irsCJump{binNE, &ireConst{0}, exprcode, l.iftrue, l.iffalse},
+		&ireConst{0},
+	}
+}
+
+func translateCExprBinop(e expr, l *exprLabels) irnode {
+	return &irsNop{}
+}
+
+func translateCExprUnop(e expr, l *exprLabels) irnode {
 	return &irsNop{}
 }
 
@@ -167,21 +236,123 @@ func translateExpr(e expr) irnode {
 }
 
 func translateExprConst(e *expConst) irnode {
-	return &irsNop{}
+	switch e.t {
+	case primBool:
+		if e.b {
+			return &ireConst{1}
+		}
+		return &ireConst{0}
+
+	case primInt:
+		return &ireConst{e.i}
+	}
+	panic("unhandled const")
 }
 
 func translateExprBinop(e *expBinop) irnode {
-	return &irsNop{}
+
+	switch e.op {
+	case binLE, binLT, binGT, binGE, binNE, binEQ:
+
+		result := newTempReg("")
+		iftrue := newTempLabel("")
+		iffalse := newTempLabel("")
+
+		return &ireESeq{
+			&irsSeq{[]irnode{
+				&irsMove{result, &ireConst{0}},
+				&irsCJump{e.op, translateExpr(e.left), translateExpr(e.right), iftrue, iffalse},
+				iftrue,
+				&irsMove{result, &ireConst{1}},
+				iffalse,
+			}},
+			result,
+		}
+	case binADD, binFDIV, binMUL, binSUB, binMOD, binDIV:
+		return &ireBinop{e.op, translateExpr(e.left), translateExpr(e.right)}
+
+	case binOR:
+		btest := newTempLabel("")
+		result := newTempReg("")
+		setResult := newTempLabel("")
+		out := newTempLabel("")
+
+		return &ireESeq{
+			&irsSeq{[]irnode{
+				&irsMove{result, &ireConst{0}},
+				&irsCJump{binNE, &ireConst{0}, translateExpr(e.left), setResult, btest},
+				btest,
+				&irsCJump{binNE, &ireConst{0}, translateExpr(e.right), setResult, out},
+				setResult,
+				&irsMove{result, &ireConst{1}},
+				out,
+			}},
+			result,
+		}
+	case binAND:
+		btest := newTempLabel("")
+		out := newTempLabel("")
+
+		result := newTempReg("")
+		setResult := newTempLabel("")
+
+		return &ireESeq{
+			&irsSeq{[]irnode{
+				&irsMove{result, &ireConst{0}},
+				&irsCJump{binEQ, &ireConst{0}, translateExpr(e.left), out, btest},
+				btest,
+				&irsCJump{binEQ, &ireConst{0}, translateExpr(e.right), out, setResult},
+				setResult,
+				&irsMove{result, &ireConst{1}},
+				out,
+			}},
+			result,
+		}
+	case binArrayIndex:
+		arr := translateExpr(e.left)
+
+		if _, ok := arr.(*ireMem); !ok {
+			panic("array not memory reference")
+		}
+
+		arrTyp := e.left.Type().(typArray)
+
+		idx := translateExpr(e.right)
+
+		// a[i] => *(a + (i-start)*sizeof(a[0]))
+		//      => *(a + i*sizeof(a[0]) - start*sizeof(a[0]))
+		return &ireMem{
+			&ireBinop{binADD,
+				arr,
+				&ireBinop{binSUB,
+					&ireBinop{binMUL, idx, &ireConst{arrTyp.typ.Size()}},
+					&ireBinop{binMUL, &ireConst{arrTyp.start}, &ireConst{arrTyp.typ.Size()}},
+				},
+			},
+		}
+	}
+
+	panic("unhandled binop: " + e.op.String())
+
 }
+
 func translateExprUnop(e *expUnop) irnode {
 	return &irsNop{}
 }
 func translateExprId(e *expId) irnode {
-	return &irsNop{}
+
+	varid := e.bound.(*varId)
+	if varid.byReference {
+		return &ireMem{varid.where}
+	}
+
+	return varid.where
 }
 func translateExprCall(e *expCall) irnode {
 	return &irsNop{}
 }
 func translateExprField(e *expField) irnode {
-	return &irsNop{}
+	record := translateExpr(e.e)
+	offs := e.field.bound.(*varId).where
+	return &ireMem{&ireBinop{binADD, record, offs}}
 }
